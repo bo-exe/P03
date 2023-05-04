6c527a2a710d9836896026355afe4371a4c053fb
boyang <22020749@myrp.edu.sg>
2023-05-04 12:27:54 +0800

    .

--- app/src/main/java/sg/edu/rp/c346/id22020749/p03/MainActivity.java
+++ app/src/main/java/sg/edu/rp/c346/id22020749/p03/MainActivity.java
@@ -6,13 +6,17 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.RadioGroup;
 import android.widget.TextView;
+import android.widget.ToggleButton;
 
 public class MainActivity extends AppCompatActivity {
 
     TextView tvDisplay;
     Button btnDisplay;
     EditText etInput;
+    ToggleButton tbtn;
+    RadioGroup rgGender;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -22,14 +26,37 @@ public class MainActivity extends AppCompatActivity {
         tvDisplay = findViewById(R.id.textViewDisplay);
         btnDisplay = findViewById(R.id.buttonDisplay);
         etInput = findViewById(R.id.editTextInput);
+        tbtn = findViewById(R.id.toggleButtonEnabled);
+        rgGender = findViewById(R.id.radioGroupGender);
 
         btnDisplay.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 // Code for the action
                 String stringResponse = etInput.getText().toString();
+
+                int checkedRadioId = rgGender.getCheckedRadioButtonId();
+                if(checkedRadioId == R.id.radioButtonGenderMale){
+                    // Write the code when male selected
+                    String maleResponse = "He says" + stringResponse;
+                }
+                else{
+                    // Write the code when female selected
+                    String femaleResponse = "She says" + stringResponse;
+                }
+
                 tvDisplay.setText(stringResponse);
 
+
+            }
+        });
+
+        tbtn.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View View) {
+                boolean isChecked = tbtn.isChecked();
+                if (isChecked) {
+                    etInput.setEnabled(true);
+                }
             }
         });
     }
